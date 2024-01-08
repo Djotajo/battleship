3c2f5b8190eb5ff97eba2b66b17e1a16e0cf1f67
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-01-08 13:47:57 +0100

    Implement isHit function

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,22 +4,29 @@ test("length", () => {
   expect(new Ship(5).length).toEqual(5);
 });
 
-test("if the ship is sunk", () => {
+test("if the new ship is sunk", () => {
   expect(new Ship(5).isSunk()).toEqual(false);
 });
 
-test("if the ship is sunk", () => {
+test("if the new ship is hit", () => {
   expect(new Ship(5).hits).toEqual(0);
 });
 
-test("if the testShip is sunk", () => {
+test("if the testShip got hit", () => {
   let testShip1 = new Ship(5);
-  testShip1.hits = 5;
-  expect(testShip1.isSunk()).toEqual(true);
+  testShip1.isHit();
+  expect(testShip1.hits).toEqual(1);
 });
 
-test("if the testShip got hit", () => {
+test("if the testShip is sunk, 3/5", () => {
   let testShip2 = new Ship(5);
-  testShip2.isHit();
-  expect(testShip2.hits).toEqual(1);
+  testShip2.hits = 3;
+  expect(testShip2.isSunk()).toEqual(false);
+});
+
+test("if the testShip is sunk, 4/5 + a hit", () => {
+  let testShip3 = new Ship(5);
+  testShip3.hits = 4;
+  testShip3.isHit();
+  expect(testShip3.isSunk()).toEqual(true);
 });
